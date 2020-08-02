a849953eb760450b05f8d170a742e991343c2cae
Matej Vitásek <vektor330@gmail.com>
2020-08-02 19:47:01 +0200

    Initial commit.

---
+++ data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0011201",
+      "name": "R0011201",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.6843681300170275,
+        "pitch": 0.28680337242727383,
+        "fov": 1.3868826872995463
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09232070117461433,
+          "pitch": 0.1607473042528973,
+          "rotation": 6.283185307179586,
+          "target": "1-r0011200"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r0011200",
+      "name": "R0011200",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
